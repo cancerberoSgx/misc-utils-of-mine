eff30d1840398598a041c6498e86a087d0afa59a
Sebastian Gurin <sebastigurin@gmail.com>
2018-12-29 17:15:31 -0300

    string changeText

---
+++ README.md
@@ -0,0 +1,3 @@
+Miscellaneous utilities for JavaScript/TypeScript 
+
+...that I have to include in all my projects so I'm packing all of them here. 
---
+++ spec/string/changeTextSpec.ts
@@ -0,0 +1,24 @@
+import { changeText } from "../../src";
+
+describe('changeText', () => {
+  it('add only', () => {
+    const s = `hello how are you? I hope you are fine`
+    const s2 = changeText(s, [{ pos: 5, toAdd: ' seba' }, { pos: 17, toAdd: ' today' }, { pos: 33, toAdd: ' really' }])
+    // console.log(s2);
+    expect(s2).toBe('hello seba how are you today? I hope you are really fine')
+
+  })
+  it('remove only', () => {
+    const s = `hello how are you? I hope you are fine`
+    const s2 = changeText(s, [{ pos: 6, toRemove: 'how ' }, { pos: 20, toRemove: 'hope ' }])
+    // console.log(s2);
+    debugger
+    expect(s2).toBe('hello are you? I you are fine')
+  })
+  it('remove and add', () => {
+    const s = `hello how are you? I hope you are fine`
+    const s2 = changeText(s, [{ pos: 6, toRemove: 'how ', toAdd: 'cómo ' }, { pos: 21, toRemove: 'hope ', toAdd: 'think ' }])
+    // console.log(s2);
+    expect(s2).toBe('hello cómo are you? I think you are fine')
+  })
+})
