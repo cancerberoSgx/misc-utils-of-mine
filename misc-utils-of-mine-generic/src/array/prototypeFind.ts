221ae33436646d03eecf13efe41ce7306b129b8c
Sebastian Gurin <sebastigurin@gmail.com>
2019-02-23 03:36:18 -0300

    prototypeFind

--- misc-utils-of-mine-generic/src/array/prototypeFind.ts
+++ misc-utils-of-mine-generic/src/array/prototypeFind.ts
@@ -1,4 +1,4 @@
-export function arrayPrototypeFind<T>(a: T[], predicate: FindPredicate<T>, thisArg?: any): T | undefined {
+export function arrayPrototypeFind<T, E=any>(this: E, a: T[], predicate: FindPredicate<T, typeof thisArg>, thisArg?: E): T | undefined {
   for (let i = 0; i < a.length; i++) {
     const v = a[i];
     if (predicate.apply(thisArg, [v, i, a])) {
@@ -7,17 +7,17 @@ export function arrayPrototypeFind<T>(a: T[], predicate: FindPredicate<T>, thisA
   }
 }
 
-type FindPredicate<T> = (this: void, value: T, index: number, obj: T[]) => value is T
+type FindPredicate<T,  E=any> =   (this: E, value: T, index: number, obj: T[]) => boolean
 
 declare global {
   interface Array<T> {
-    find<S extends T>(predicate: FindPredicate<T>, thisArg?: any): S | undefined;
-    find(predicate: (value: T, index: number, obj: T[]) => boolean, thisArg?: any): T | undefined;
+    find<S extends T>(predicate: FindPredicate<T, typeof thisArg>, thisArg?: any): S | undefined;
+    find(predicate: FindPredicate<T, typeof thisArg>, thisArg?: any): T | undefined;
   }
 }
 export function installArrayPrototypeFind(force=false) {
-  Array.prototype.find = (typeof Array.prototype.find === 'undefined'||force) ? function < T>(predicate: FindPredicate<T>, thisArg?: any): T | undefined { 
-    //@ts-ignore
+  Array.prototype.find = (typeof Array.prototype.find === 'undefined'||force) ? function < T>(this: Array<T>,predicate: FindPredicate<T, typeof thisArg>, thisArg?: any): T | undefined { 
+    //@ts- ignore
     return arrayPrototypeFind(this, predicate, thisArg); 
   } : Array.prototype.find;
 }
