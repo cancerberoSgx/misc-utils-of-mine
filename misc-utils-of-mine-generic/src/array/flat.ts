96687e6fd30f971a18f7d034845f1fa461c7110b
Sebastian Gurin <sebastigurin@gmail.com>
2019-02-23 02:26:37 -0300

    unlink

--- misc-utils-of-mine-generic/src/array/flat.ts
+++ misc-utils-of-mine-generic/src/array/flat.ts
@@ -5,3 +5,7 @@ export function flatDeep<T = any>(arr1: T[][] | T[]): T[] {
 export function flat<T>(arr: T[][]): T[] {
   return arr.reduce((a, b) => a.concat(b))
 }
+
+export function flatReadOnly<T>(arr: ReadonlyArray<ReadonlyArray<T>>): ReadonlyArray<T> {
+  return arr && arr.length ? arr.reduce((a, b) => a.concat(b)) : []
+}
