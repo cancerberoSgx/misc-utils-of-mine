977463bdb091037d4813eb113f89064faa91f070
Sebastian Gurin <sebastigurin@gmail.com>
2019-02-25 09:26:47 -0300

    wrapInHtml

--- misc-utils-of-mine-dom/src/html.ts
+++ misc-utils-of-mine-dom/src/html.ts
@@ -1,21 +1,4 @@
 
-export function wrapInHtml(s: string): string {
-  return `
-  <!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>title</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-${s}
-</body>
-</html>
-`;
-}
-
 /** sets innerHTML and calls children scripts if any */
 export function setInnerHTML(elm: HTMLElement, html: string) {
   elm.innerHTML = html;
--- misc-utils-of-mine-generic/src/string/html.ts
+++ misc-utils-of-mine-generic/src/string/html.ts
@@ -4,3 +4,20 @@ export function escapeHtmlAttribute(code: string) {
 export function unEscapeHtmlAttribute(code: string) {
   return code.replace(/\&quot\;/gmi, '"');
 }
+
+export function wrapInHtml(s: string): string {
+  return `
+  <!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>title</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+${s}
+</body>
+</html>
+`;
+}
