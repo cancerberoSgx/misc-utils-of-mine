21d30fc8aa00d225984745637bfdc6fe20091701
Sebastián Gurín <sgurin@awalabs.com>
2019-04-13 13:51:32 -0300

    enum

--- misc-utils-of-mine-generic/src/array/array.ts
+++ misc-utils-of-mine-generic/src/array/array.ts
@@ -25,3 +25,29 @@ export function seq(start: number = 0, step: number = 1, max: number = 0): numbe
   }
   return result
 }
+/** 
+ * List given enum keys as array. Must be used on enums with declared values (enum E {a='a', b='b'}, if not values could be also returned . For enums without declared values, use enumNoValueKeys
+ */
+export function enumKeys(anEnum: any): string[] {
+  const a = []
+  for (let i in anEnum) {
+    a.push(i)
+  }
+  return a
+}
+
+/** 
+ * List given enum keys as array. Differently to [[enumKeys]], is should be used only on enums that doesn't have assigned values 
+ * or other wise on those which values are identical to the keys or not strings. If not, they will be returned also! 
+ */
+export function enumNoValueKeys(anEnum: any): string[] {
+  return Object.keys(anEnum).map(i=>anEnum[i as any]).filter((s, i, a)=>typeof s === 'string' && a.indexOf(s)===i)
+  }
+
+  
+/**
+ * return the Enum type from given string enum key obtained with key [[enumNoValueKeys]]
+ */
+function enumValueFromString<T>(key: string, anEnum: any): T | undefined {
+  return anEnum[key]
+}
--- misc-utils-of-mine-generic/src/object.ts
+++ misc-utils-of-mine-generic/src/object.ts
@@ -1,8 +1,11 @@
 import { EmptyObject, ObjectStringKeyUnion } from 'misc-utils-of-mine-typescript'
-let object
+
 export function objectKeys<Field extends EmptyObject = EmptyObject>(o: Field): ObjectStringKeyUnion<Field>[] {
   return Object.keys(o) as ObjectStringKeyUnion<Field>[]
 }
+/**
+ * Returns a new object with the same keys of given one, and values mapped with given function
+ */
 export function objectMap(
   o: {
     [k: string]: any
@@ -14,4 +17,59 @@ export function objectMap(
     r[k] = f(k, o[k])
   })
   return r
+}
+
+/**
+ * build an object using keys in [[a]] and values returning from [[fn]] as long as they are not undefined
+ */
+export function arrayToObject<T=any>(a: string[], fn: (a:string)=>T|undefined){
+  const o :{[s:string]:T} = {}
+  a.forEach(k=>{
+    const v = fn(k)
+    if(typeof v !== 'undefined'){
+      o[k] = v
+    }
+  })
+  return o
+}
+/**
+ * Returns a nested property of given object and given path. For example path could be 'foo.bar' and it will return `object['foo']['bar']`
+ */
+export function getObjectProperty<T>(object: any, path: string, default_value: T): T | undefined {
+  if (!path) {
+    return object;
+  }
+  else if (object) {
+    var tokens = path.split('.')
+      , prev = object
+      , n = 0;
+    while (typeof prev !== 'undefined' && n < tokens.length) {
+      prev = prev[tokens[n++]];
+    }
+    if (typeof prev !== 'undefined') {
+      return prev;
+    }
+  }
+  return default_value;
+}
+
+export function setProperty<T>(object: any, path: string, value: T) {
+  if (!path) {
+    return;
+  }
+  else if (!object) {
+    return;
+  }
+  var tokens = path.split('.')
+    , prev = object;
+  for (var token_idx = 0; token_idx < tokens.length - 1; ++token_idx) {
+    var current_token = tokens[token_idx];
+    if (typeof prev[current_token] === 'undefined') {
+      prev[current_token] = {};
+    }
+    prev = prev[current_token];
+  }
+  if (tokens.length) {
+    prev[tokens[tokens.length - 1]] = value
+  }
 }
