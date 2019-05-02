cb2e71a32870cbefc446a6d85dafa79d56562ee9
Sebastián Gurín <sgurin@awalabs.com>
2019-05-02 17:55:03 -0300

    emitter

--- misc-utils-of-mine-generic/src/event.ts
+++ misc-utils-of-mine-generic/src/event.ts
@@ -1,5 +1,7 @@
-/** Simpler event emitter. */
-export class Emitter<E = any, L extends (e: E) => void = (e: E) => void> {
+/** 
+ * The simplest implementation of an event emitter. 
+ */
+export class Emitter<E = any, L extends Listener<E> = Listener<E>> {
   private l: L[] = []
   add(l: L) {
     this.l.push(l)
@@ -7,4 +9,8 @@ export class Emitter<E = any, L extends (e: E) => void = (e: E) => void> {
   emit(e: E) {
     this.l.forEach(l => l(e))
   }
+  remove(l: L) {
+    this.l = this.l.filter(a=>a===l)
+  }
 }
+type Listener<E> = (e: E) => void
