3eedac50c8867301442f9cc72b1736fe2d251919
Sebastián Gurín <sgurin@awalabs.com>
2019-08-23 17:17:58 -0300

    hash impl for jsdom

--- misc-utils-of-mine-generic/src/string/hash.ts
+++ misc-utils-of-mine-generic/src/string/hash.ts
@@ -1,9 +1,24 @@
-export function hashCode(s: string) {
-  let h = 0
-  for (let i = 0, h = 0; i < s.length; i++) {
-    h = Math.imul(31, h) + s.charCodeAt(i) | 0
+
+/**
+ * Returns a hash code for a string.
+ * (Compatible to Java's String.hashCode())
+ *
+ * The hash code for a string object is computed as
+ *     `s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]`
+ * using number arithmetic, where s[i] is the i th character
+ * of the given string, n is the length of the string,
+ * and ^ indicates exponentiation.
+ * (The hash value of the empty string is zero.)
+ *
+ * @param {string} s a string
+ * @return {number} a hash code value for the given string.
+ */
+function hashCode(s: string) {
+  var h = 0, l = s.length, i = 0;
+  if (l > 0) {
+    while (i < l) {
+      h = (h << 5) - h + s.charCodeAt(i++) | 0;
+    }
   }
-  return h
+  return h;
 }
-
-
